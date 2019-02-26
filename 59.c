701a6c4e0ddf42eddc069e1bd5ca0803e5db71d4
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-26 12:14:48 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,19 @@
+# set6
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,max=0,i,arr[10];
+max=a[0];
+for(i=0;i<10;i++)
+{
+scanf("%d\n",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+if(arr[i]>max)
+max=arr[i];
+}
+printf("%d\n",max);
+return 0;
+}

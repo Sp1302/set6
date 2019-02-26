d0fc55167e61f48364d5a7fb132681f78379322e
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-26 12:08:23 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,17 @@
+# set6
+#include<stdio.h>
+int main()
+{
+char str;
+printf("\nenter the string");
+gtes(str);
+if((str>a)&&(str<z)||(str>0)&&(str<9))
+{
+printf("\nit contains alphabet and numbers");
+}
+else
+{
+printf("\nno numbers and alphabets");
+}
+return 0;
+}

8a97f6843759dced6123a69e2a78e02ecda02f2d
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-26 12:04:16 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,17 @@
+# set6
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\nenter the number");
+scanf("%d\n",&n);
+if(n%2==0)
+{
+printf("%d\n",n);
+}
+else
+{
+printf("%d\n",n-1);
+}
+return 0;
+}
